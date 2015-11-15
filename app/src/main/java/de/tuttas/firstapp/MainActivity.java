79cfc3e0e2891e4fe0320e6858fcf88ccf65177b
jtuttas <tuttas@mmbbs.de>
2015-11-15 14:00:27 +0100

    Initial commit

---
+++ app/src/main/java/de/tuttas/firstapp/MainActivity.java
@@ -0,0 +1,71 @@
+package de.tuttas.firstapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // TODO Auto-generated method stub
+        msg("onSaveInstanceState()");
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        msg("onCreate()");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        msg("onDestroy()");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        msg("onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart() {
+        // TODO Auto-generated method stub
+        msg("onRestart()");
+
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        msg("onResume()");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        msg("onStart()");
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        // TODO Auto-generated method stub
+        msg("onStop()");
+        super.onStop();
+    }
+
+    private void msg(String msg) {
+        // TODO Auto-generated method stub
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
